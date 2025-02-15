bec5790ce357bfbc116b1777e522729e78f80054
mdaffanwd <mohammedaffan2305@gmail.com>
2025-02-15 12:39:27 +0530

    add mdlwrs in app.js

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ import { authRouter } from './routes/auth.routes.js';
 import { subscriptionRouter } from './routes/subscription.routes.js';
 import { connectToDatabase } from './db/mongodb.js';
 import { errorMiddleware } from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
